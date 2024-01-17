4c0bac8442cd47251fa16d47ca8e0d1f674967b2
Peter Richards <petrich@microsoft.com>
2024-01-16 16:01:35 -0800

    Switch to Minimal API and add stub warranties operation

---
+++ ContosoComputers/Models/WarrantyInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace ContosoComputers.Models;
+
+internal class WarrantyInfo
+{
+    public string SerialNumber { get; set; }
+    public DateTime WarrantyExpires { get; set; }
+}
--- ContosoComputers/Program.cs
+++ ContosoComputers/Program.cs
@@ -1,3 +1,7 @@
+using ContosoComputers.Models;
+
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,10 +13,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
@@ -20,6 +25,25 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapGet("/warranties/{serialNum}", (string serialNum) =>
+{
+    return Results.Ok(new WarrantyInfo
+    {
+        SerialNumber = serialNum,
+        WarrantyExpires = DateTime.Now.AddDays(30)
+    });
+})
+.WithName("GetWarrantyInfo")
+.Produces<WarrantyInfo>(StatusCodes.Status200OK)
+.WithDescription("Get the warranty information for a device based on serial number\n#ExamplePrompt Is the warranty valid for this device")
+.WithOpenApi((op) =>
+{
+    var serialNumParam = op.Parameters.FirstOrDefault(p => p.Name == "serialNum");
+    if (serialNumParam != null)
+    {
+        serialNumParam.Description = "The serial number of the device";
+    }
+    return op;
+});
 
 app.Run();
